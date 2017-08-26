f394c71330436e64ad47b036ec5cbe9d77eecbf9
Matti Jokitulppo <melonmanchan@gmail.com>
2017-08-26 18:50:06 +0300

    Fix async/await

--- src/handler.js
+++ src/handler.js
@@ -1,30 +1,13 @@
 import CPD from 'chrome-remote-interface'
 
-module.exports.render = async (event, context, callback, chrome) => {
-  try {
-    const asd = await CPD.Version()
-  } catch (error) {
-      callback(null, { error })
-    return
-  }
+export const render = async (event, context, callback, chrome) => {
+  const versionInfo = await CPD.Version()
 
-  CDP.Version()
-    .then((versionInfo) => {
-      callback(null, {
-        statusCode: 200,
-        body: JSON.stringify({
-          versionInfo,
-          asd,
-          chrome,
-        }),
-      })
-    })
-    .catch((error) => {
-      callback(null, {
-        statusCode: 500,
-        body: JSON.stringify({
-          error,
-        }),
-      })
-    })
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      versionInfo,
+      chrome,
+    }),
+  })
 }
